8f3e1aa08e8eba3dafb6d2410c695f2d2db78abc
Richard Choquerive Ramos <kurivavilo@gmail.com>
2025-07-05 16:38:29 -0400

    Clase sesión 11

---
+++ sesion11.py
@@ -0,0 +1,216 @@
+#print ("Diccionario de clave entera y valor entero")
+#diccionario = {1: 10, 2: 20, 3: 30}
+#print(diccionario)
+#print(type(diccionario))
+#print(diccionario[1])
+
+#print ("Diccionario de clave cadena y valor entero")
+#diccionario = {'uno': 1, 'dos': 2, 'tres': 3}
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Diccionario de clave cadena y valor cadena")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Diccionario mixto")
+#diccionario = {
+#    1:"ID-XXXXX", 
+#    "edad": 3, 
+#    'animal': '🐶' , 
+#    ("John","Doe"): 6917222722, 
+#    "salvaje": False,
+#    1:"ID-1XXXX", 
+#    }
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Diccionario vacío")
+#diccionario = {}
+#print(diccionario)
+#print(type(diccionario))
+#diccionario = dict()
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Diccionario a partir de una lista")
+#lista = [['perro', '🐶'] , ['gato','🐱'] , ['ave','🐦']]
+#print(lista)
+#diccionario = dict(lista)
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Diccionario a partir de una tupla de tuplas")
+#tupla = (('perro', '🐶') , ('gatos','🐱') , ('ave',['🐦','🦅']))
+#print(tupla)
+#diccionario = dict(tupla)
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Diccionario mediante comprensión")
+#diccionario = {animal:animal*3 for animal in '🐶🐱🐹🐰'}
+#print(diccionario)
+#print(type(diccionario))
+
+#print ("Acceder mediante clave")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#print(diccionario['perro'],type(diccionario['perro']))
+#print(diccionario['gato'],type(diccionario['gato']))
+#print(diccionario['ave'],type(diccionario['ave']))
+
+#print ("Cambiar el valor de una clave")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#diccionario['perro'] = '🐩'
+#print(diccionario)
+
+#print ("Crear un nuevo par clave-valor")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#diccionario['pez'] = '🐠'
+#print(diccionario)
+
+#print ("Eliminar un par clave-valor")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#del diccionario['ave']
+#print(diccionario)
+
+#print ("Modificar la clave")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#diccionario['perrito'] = diccionario['perro']
+#del diccionario['perro']
+#print(diccionario)
+
+#print ("Concatenar diccionarios")
+#diccionario1 = {'perro': '🐶', 'gato': '🐱'}
+#diccionario2 = {'ave': '🐦', 'pez': '🐠'}
+#concatenado = diccionario1 + diccionario2 # TypeError: unsupported operand type(s) for +: 'dict' and 'dict'
+
+#print ("Repetir diccionarios")
+#diccionario = {'perro': '🐶', 'gato': '🐱'}
+#repetido = diccionario * 3 # TypeError: unsupported operand type(s) for *: 'dict' and 'int'
+
+#print ("Método get(clave)")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print (diccionario)
+#perro = diccionario.get('perro')
+#print(perro, type(perro))
+#pez = diccionario.get('pez')
+#print(pez, type(pez))
+
+#print ("Método items()")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#items = diccionario.items()
+#print(items, type(items))
+#items = list(items) # Convertir a lista
+#print(items, type(items))
+#print(items[0], type(items[0]))
+
+#print ("Método keys()")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#keys = diccionario.keys()
+#print(keys, type(keys))
+#keys = list(keys) # Convertir a lista
+#print(keys, type(keys))
+#print(keys[0], type(keys[0]))
+
+#print ("Método values()")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#values = diccionario.values()
+#print(values, type(values))
+#values = list(values) # Convertir a lista
+#print(values, type(values))
+#print(values[0], type(values[0]))
+
+#print ("Método update(diccionario)")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#diccionario.update({'pez': '🐠', 'perro': '🐩'})
+#print(diccionario)
+
+#print ("Método update(clave=valor)")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#diccionario.update(pez='🐠', perro='🐩')
+#print(diccionario)
+
+#print ("Método clear()")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#diccionario.clear()
+#print(diccionario)
+
+#print ("Método pop(clave)")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#gato = diccionario.pop('gato')
+#print(gato, type(gato))
+#print(diccionario)
+
+#print ("Método popitem()")
+#diccionario = {'perro': '🐶', 'gato': '🐱'}
+#print(diccionario)
+#par = diccionario.popitem()
+#print(par, type(par))
+#print(diccionario)
+
+#print ("Asignación por referencia")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#copia = diccionario
+#print(copia)
+#copia['ave'] = '🦅'
+#print(diccionario)
+#print(copia)
+
+#print ("Asignación por referencia")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#copia = diccionario.copy()  # Asignación por copia para evitar referencias
+#print(copia)
+#copia['ave'] = '🦅'
+#print(diccionario)
+#print(copia)
+
+#print ("Función len()")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#longitud = len(diccionario)
+#print(longitud)
+
+#print ("Función in  / not in")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#existe = 'perro' in diccionario
+#print(existe, type(existe))
+#no_existe = 'pez' not in diccionario
+#print(no_existe, type(no_existe))
+
+#print ("Función iter()")
+#diccionario = {'perro': '🐶', 'gato': '🐱', 'ave': '🐦'}
+#print(diccionario)
+#iterador = iter(diccionario.items())
+#print(type(iterador))
+#siguiente = next(iterador)
+#print(siguiente, type(siguiente))
+#siguiente = next(iterador)
+#print(siguiente, type(siguiente))
+#siguiente = next(iterador)
+#print(siguiente, type(siguiente))
+
+print ("Diccionarios anidados")
+diccionario = {'perro': '🐶', 'gato': '🐱',  'ave': {'pajaro': '🐦', 'aguila': '🦅'}}
+print(diccionario)
+aves = diccionario['ave']
+print(aves)
+ave = aves['pajaro']
+print(ave)
+ave = aves['aguila']
+print(ave)
