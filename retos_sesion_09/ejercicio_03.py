084d5a94e523d111ee15234606177e4ce32daf0d
Richard Choquerive Ramos <kurivavilo@gmail.com>
2025-08-14 20:04:59 -0400

    correccion de retos sesion 9

--- retos_sesion_09/ejercicio_03.py
+++ retos_sesion_09/ejercicio_03.py
@@ -2,8 +2,8 @@
 
 lista_nombres = ['Ana', 'Luis', 'Pedro', 'José', 'Juan', 'Laura', 'Carlos', \
     'Marta', 'Javier', 'Lucía']
-sub_lista = lista_nombres[5:9:2]
-print(f'Sublista de los indices 5 a 9 con saltos de 2: {sub_lista}')
+sub_lista = lista_nombres[5:10:2]
+print(f'Sublista de 5 a 9 con saltos de 2: {sub_lista}')
 posicion_jose = lista_nombres.index('José')
 print(f'Posición de "José": {posicion_jose}')
 lista_nombres.sort()
